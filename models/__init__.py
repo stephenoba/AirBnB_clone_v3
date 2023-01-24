3eaf8dd6036b11c876107a1fec6e6eef8b649536
Stephen Oba <obastepheno@gmail.com>
2023-01-24 13:58:52 +0100

    remove print statements

--- models/__init__.py
+++ models/__init__.py
@@ -10,10 +10,8 @@ storage_t = getenv("HBNB_TYPE_STORAGE")
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
-    print("using db storage")
     storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
-    print("using file storage")
     storage = FileStorage()
 storage.reload()
