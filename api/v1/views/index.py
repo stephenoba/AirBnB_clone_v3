2f065c9f6f897d1c24a0c2715a2ce973cdac8784
Stephen Oba <obastepheno@gmail.com>
2023-01-24 15:52:44 +0100

    Add /stats end point

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,27 @@
 """index.py to connect to API"""
 from flask import jsonify
 
+from models import storage
 from api.v1.views import app_views
+
+
+entities = ["Amenity", "City", "Place", "Review", "State", "User"]
 
 
 @app_views.route('/status', strict_slashes=False)
 def getStatus():
     """Get status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def getStats():
+    """Get stats"""
+    res = {}
+    for e in entities:
+        res[e.lower()] = storage.count(e)
+    return jsonify(res)
+
+
+if __name__ == "__main__":
+    pass
