a4204cc543b3d6fe9cee64ce6109b7e1c507e95a
Stephen Oba <obastepheno@gmail.com>
2023-01-25 16:24:26 +0100

    add 404 page

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """flask app"""
 import os
 
-from flask import Flask
+from flask import Flask, make_response, jsonify
 
 from models import storage
 from api.v1.views import app_views
@@ -15,6 +15,11 @@ app.register_blueprint(app_views)
 def teardown_appcontext(code):
     """teardown_appcontext"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
